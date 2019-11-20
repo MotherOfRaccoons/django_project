a29441a10bde20479056a8a4b53f3cb97575c7cf
MotherOfRaccoons <mr.bronium@gmail.com>
2019-11-20 13:43:50 +0300

    minor changes

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+import django
+
+django.setup()
 
 
 urlpatterns = [
